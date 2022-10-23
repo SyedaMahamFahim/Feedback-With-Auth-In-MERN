6b8861d2e6e885b2127d7c1a8e7e80d25fee8aac
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-10-23 21:06:41 +0500

    added timestamp

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,32 +1,31 @@
 const mongoose = require("mongoose");
 
-const todoModel = mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
+const todoModel = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    status: {
+      type: String,
+      required: [true, "Status is required"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Description is required"],
-    trim: true,
-  },
-  status: {
-    type: String,
-    required: [true, "Status is required"],
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: [true, "User is required"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Todo", todoModel);
-
-
-
-
